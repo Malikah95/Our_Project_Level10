759e94a75fb3bd65a256ac324d42da2d2ad70509
sunduss abdullah <s.alghernas1414@hotmail.com>
2018-09-26 05:48:06 +0300

    add popup

--- Sym.py
+++ Sym.py
@@ -1,14 +1,21 @@
 import kivy
 from kivy import Config
 from kivy.lang import Builder
-
+from kivy.uix.popup import Popup
 kivy.require('1.10.0')
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-
+import mysql.connector
+import mysql
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
+from mysql.connector import MySQLConnection
+from kivy.uix.screenmanager import Screen
 
 mydb =mysql.connector.Connect(
 
@@ -17,8 +24,6 @@ mydb =mysql.connector.Connect(
     password="root1234567",
     database="user_inf"
  )
-
-
 cur = mydb.cursor()
 
 Config.set('graphics', 'width',  350)
@@ -29,48 +34,57 @@ Builder.load_file("symkivy.kv")
 class Manager(ScreenManager):
     pass
 
-class LoginWindow(Screen):
+class LoginWindow(Widget):
 
 
         def Login_pressed(self):
             username_text = self.username_input.text
             password_text = self.password_input.text
+            popup1 = Popup(content=Label(text='Enter a valid username and password'),
+                           auto_dismiss=True, size_hint=(None, None), size=(400, 400))
+
 
             cur.execute(
                 "SELECT username,pass FROM user WHERE username='" + username_text + "' AND pass ='" + password_text + "'")
             count = cur.fetchone()
 
             if count is None:
+                popup1.open()
 
-                # Label
-                print("Enter a valid username and password")
 
             else:
+                #diagnosis interface, delete print
                 print("correct")
 
 
 
 class Loginapp(App):
         def build(self):
-            self.load_kv('log.kv')
-            return Login()
+            self.load_kv('symkivy.kv')
+            return LoginWindow()
 
-class Register(Screen):
+class Register(Widget):
     def register_pressed(self):
         username_text = self.username_input.text
         email_text = self.email_input.text
         password_text = self.password_input.text
+        popup1 = Popup(content=Label(text='Username is exists,please change'),
+                       auto_dismiss=True, size_hint=(None, None), size=(400, 400))
+
+        popup2 = Popup(content=Label(text='Fill all blank'),
+                       auto_dismiss=True, size_hint=(None, None), size=(400, 400))
 
         cur.execute("SELECT * FROM user WHERE username='" + username_text + "'")
         count = cur.fetchone()
 
         if count is not None:
-            print("Username is exists,please change...")
+            popup1.open()
+
 
         elif not username_text or not email_text or not password_text:
+            popup2.open()
 
-            # need LABEL
-            print("must fill all blank.")
+
 
 
 
@@ -86,8 +100,8 @@ class Register(Screen):
 
 class RegesApp(App):
     def build(self):
-        self.load_kv('reges.kv')
-        return Regesration()
+        self.load_kv('symkivy.kv')
+        return Register()
 
 
 RegesApp().run()
@@ -97,9 +111,9 @@ class Personal_Info(Screen):
 
         def personal_Info(self):
             age_input = self.age_input
-            Gender_input = self.Gender_input
-            BloodPresure_input = self.BloodPresure_input
-            Length_input = self.Length_input
+            gender_input = self.gender_input
+            bloodpresure_input = self.bloodpresure_input
+            length_input = self.length_input
             weight_input = self.weight_input
 
             cur.execute("SELECT age_input,Gender_input,BloodPresure_input,weight_input,Length_input  FROM user")
@@ -108,8 +122,8 @@ class Personal_Info(Screen):
 
             if count is None:
                 cur.execute(
-                    "INSERT INTO user (Age,Gender,BloodPresure,Weight,Length )VALUES('" + age_input + "', '" + Gender_input + "', '" + BloodPresure_input + "',"
-                                                             " '" + weight_input + "', '" + Length_input + "')")
+                    "INSERT INTO user (Age,Gender,BloodPresure,Weight,Length )VALUES('" + age_input + "', '" + gender_input + "', '" + bloodpresure_input + "',"
+                                                             " '" + weight_input + "', '" + length_input + "')")
                 mydb.commit()
                 cur.close()
                 mydb.close()
@@ -117,7 +131,7 @@ class Personal_Info(Screen):
 
 class personal_InfoApp(App):
     def build(self):
-        self.load_kv('reges.kv')
+        self.load_kv('symkivy.kv')
         return personal_Inf
 
 
